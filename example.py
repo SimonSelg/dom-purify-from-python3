5782266cedd0726dbd2b9ff8b4167fa3ee8283fb
Simon Selg <simon.selg@gmail.com>
2019-05-15 18:50:45 +0200

    document code

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 import subprocess
 
 def sanitizeHTML(dirty):
+  # use node to run "dist/main.js", pass the dirty HTML via stdin
   p = subprocess.run(['node', 'dist/main.js'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=False, input=dirty.encode())
   if p.stderr or p.returncode:
     return false
